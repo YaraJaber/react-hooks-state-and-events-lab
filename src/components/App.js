ae5a7e75b6f28dd58cbb6bcf4550f78350e9a5f3
YaraJaber <145011720+YaraJaber@users.noreply.github.com>
2023-10-22 15:32:03 +0400

    Completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import ShoppingList from "./ShoppingList";
 import itemData from "../data/items";
 
 function App() {
+  const [items, setItems] = useState(itemData);
+  const [isDarkMode, setIsDarkMode] = useState(false);
 
-  // replace 'false' with a state variable that can be toggled between true and false
-  // this will be used for the Dark Mode Toggle feature
-  const appClass = false ? "App dark" : "App light"
+  function handleDarkModeClick() {
+    setIsDarkMode((isDarkMode) => !isDarkMode);
+  }
+
+  const appClass = isDarkMode ? "App dark" : "App light";
+
+  // Add a new item to the shopping list
+  function addItem() {
+    const newItem = {
+      id: Date.now(), // You should use a more reliable method to generate IDs in a real application
+      name: "New Item",
+    };
+    setItems((prevItems) => [...prevItems, newItem]);
+  }
 
   return (
     <div className={appClass}>
       <header>
         <h2>Shopster</h2>
-        <button>Dark Mode</button>
+        <button onClick={handleDarkModeClick}>
+          {isDarkMode ? "Dark" : "Light"} Mode
+        </button>
+        <button onClick={addItem}>Add Item</button>
       </header>
-      <ShoppingList items={itemData} />
+      <ShoppingList items={items} />
     </div>
   );
 }
